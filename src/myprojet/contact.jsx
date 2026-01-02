1e370825ac802e0133acde3ff41e5d7d6872d5f5
AZ-EDDINEERR <azeddineerrahmaoui2@gmail.com>
2026-01-02 16:24:52 +0100

    Refactor import statements in contact.jsx

--- src/myprojet/contact.jsx
+++ src/myprojet/contact.jsx
@@ -1,6 +1,5 @@
 // src/myprojet/Forms.js
-import React from "react";
-import { useState, useEffect } from 'react'
+import React,{ useState, useEffect } from 'react'
 function Contact() {
   const [nom, setNom] = useState('');
   const [email, setEmail] = useState('');
